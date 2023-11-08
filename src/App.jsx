e52e998c82de3c0cf161ba4f3259bbf11ebbde46
Ye Min Aung <yeminaung013@gmail.com>
2023-11-08 22:12:03 +0630

    tailwindcss config setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-	return <div>Todo App</div>;
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<h1 className="text-5xl font-bold">Todo App</h1>
+		</div>
+	);
 };
 
 export default App;
